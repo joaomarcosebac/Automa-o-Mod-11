3c0ce0f7c9067a18765d20df8b7183c6c3a1c984
joao marcos <joaomarcos.20@icloud.com>
2023-04-05 01:19:22 -0300

    teste commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+
+context('funcionalidade login', () =>{
+
+
+    beforeEach(() => {
+        cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')   
+       });
+
+       
+       it.only ('Deve fazer login com sucesso', () =>{
+        cy.get ('#username').type('aluno_ebac@teste.com')
+        cy.get ('#password').type('teste@teste.com')
+        cy.get ('.woocommerce-form >.button').click()
+        
+        cy.get ('.page-title').should('contain' ,'Minha conta')
+        cy.get('a > .hidden-xs').should('contain' , 'Welcome')
+        
+            })
+        
+        it('Deve exibir uma mensagem de erro ao inserir usuario invalidos',()=>{
+            cy.get ('#username').type('ebac@teste.com')
+            cy.get ('#password').type('teste@teste.com')
+            cy.get ('.woocommerce-form >.button').click()
+            cy.get('.woocommerce-error > li').should('contain' , 'senha fornecida')
+        })
+        
+        it('Deve exibir uma mensagem de erro ao inserir senha invalida',()=>{
+            cy.get ('#username').type('aluno_ebac@teste.com')
+            cy.get ('#password').type('e@teste.com')
+            cy.get ('.woocommerce-form >.button').click()
+        })
+        
+        
+        
+        })
